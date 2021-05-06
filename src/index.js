ea1d55811bfe47b69363a49908a285f0b38489cc
JackNT <zhuxchong@gmail.com>
2021-05-06 23:42:29 +1000

    zxc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+function testable(isTestable) {
+  return function (target) {
+    target.isTestable = isTestable;
+  };
+}
+
+@testable(true)
+class MyTestableClass {}
+MyTestableClass.isTestable;
+
+//================================
+
+function testable(t) {
+  t.prototype.isTestable = true;
+}
+
+@testable(true)
+class MyTestableClass {}
+const obj = new MyTestableClass();
+obj.isTestable;
+
 //不改变原来方法，增加功能
 // class MyClass {}
 
@@ -129,3 +150,22 @@ const math = new Math();
 
 // Calling add with [2, 4]
 math.add(2, 4);
+/////////============////////////////////////////////////////
+
+//开始定义装饰器//看到两个箭头函数感觉懵逼了，转化一个也就是一个函数里返回一个函数再返回一个组件包裹器而已//title参数对应上面的“Profile Page”字符串
+//WrappedComponent参数对应上面的Profile组件
+//然后在组件加载完修改了title，在返回一个新组件，是不是很像高阶组件呢
+const setTitle = (title) => (WrappedComponent) => {
+  return class extends React.Component {
+    componentDidMount() {
+      document.title = title;
+    }
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+};
+//假如有这么一个页面组件，用于显示用户资料的，当从Home组件进去到这个组件时
+//希望title从“Home Page”变成“Profile Page”
+//注意这里隐形传入了组件，语法类似setTitle('Profile Page')(Profile)@setTitle("Profile Page")
+class Profile extends React.Component {}
