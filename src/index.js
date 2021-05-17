6297521e282f8a4ac418410a768ebe0a8d20ab65
luke <zhuxchong@gmail.com>
2021-05-17 23:20:01 +1000

    zx

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
  * @Author: Luke Z
  * @Date: 2021-05-04 23:14:21
  * @LastEditors: Luke Z
- * @LastEditTime: 2021-05-06 23:27:12
+ * @LastEditTime: 2021-05-17 23:19:27
  * @FilePath: /DesignPattern/src/index.js
  */
 
 // 适配器模式 提供不一样的接口，比如老旧功能不能用了，写个新的接口，可以接触目标类
-// 代理模式 显示原有功能，但是经过限制后者改写后
-// 装饰器模式  扩展功能，不能改写原有的
+// 代理模式 显示与原来一模一样的功能，但是功能是经过包装或者修改的，就是包了一层，改写了一下功能
+// 装饰器模式  扩展功能，不能改写原有的，包了一层 增加新的功能
 
 class RealImg {
   constructor(fileName) {
